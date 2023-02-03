eae74f277cb1ee2a52f0962ef27ee40174146a0e
Kiril <vtkkllx2367@gmail.com>
2023-02-03 19:01:45 +0300

    adaptive device page / new device schema

--- src/components/DeviceItem.tsx
+++ src/components/DeviceItem.tsx
@@ -4,18 +4,34 @@ import c from '../styles/DevicePanel.module.scss'
 
 interface DeviceItemProps {
     device: DeviceI
+    //should rewrite type
 }
 
 export default function DeviceItem({ device }: DeviceItemProps) {
-   
-//  console.log(   device.images[0].urls);
-   
+
+
+  if (device.colors) {
+    //as first photo by first color
+    console.log(device.images[device.colors[0]][0])
+  }
+
     return (
         <div className={c.device_item}>
-           <div>
-            {/* <img src={'dawdwa'}/> */}
-           </div>
-            {device.name}
+            <div className={c.image_container}>
+                <img src={device.colors? device.images[device.colors[0]][0] as string : 'image'} />
+            </div>
+            <div className={c.device_item_info_block}>
+                <p className={c.name_header}>
+                    {device.name}
+                </p>
+                <p>
+                    {device.colors ? device.faceDescription: 'wda'}
+                </p>
+            </div>
+            <div className={c.item__bottom__container}>
+               <p>{device.price}</p>
+               <button>Add</button>
+            </div>
         </div>
     )
 }
--- src/models/models.ts
+++ src/models/models.ts
@@ -8,18 +8,51 @@ export type Brands = string[]
 
 export type Brand = string
 
-interface DeviceImageItme {
-    color: string
+interface DeviceImagesArray {
+    colors: {}
     urls: string[]
+
 }
+interface imagesI {
+    colors:string
+
+}
+
+
+// "id": 1,
+// "name": "Iphone 12",
+// "brand": "apple",
+// "type": "phone",
+// "price": "24,000",
+// "colors": ["blue","purple"],
+// "description": "bla bla bla bla bla bla bla bla ",
+// "rating": "1",
+// "images": {
+//     "purple": [
+//         "https://i.allo.ua/media/catalog/product/cache/1/image/524x494/602f0fa2c1f0d1ba5e241f914e856ff9/w/w/wwru_iphone12mini_q321_purple_pdp-image-1b_result_1_1_1.jpg",
+//         "https://i.allo.ua/media/catalog/product/cache/1/image/524x494/602f0fa2c1f0d1ba5e241f914e856ff9/w/w/wwru_iphone12mini_q321_purple_pdp-image-1a_result_1_1_1.jpg",
+//         "https://i.allo.ua/media/catalog/product/cache/1/image/524x494/602f0fa2c1f0d1ba5e241f914e856ff9/w/w/wwru_iphone12mini_q321_purple_pdp-image-2_result_1_1_1.jpg",
+//         "https://i.allo.ua/media/catalog/product/cache/1/image/524x494/602f0fa2c1f0d1ba5e241f914e856ff9/w/w/wwru_iphone12mini_q321_purple_pdp-image-3_result_1_1_1.jpg"
+//     ],
+//     "blue": [
+//         "https://content.rozetka.com.ua/goods/images/big/30872903.jpg",
+//         "https://content1.rozetka.com.ua/goods/images/big/30872861.jpg",
+//         "https://content1.rozetka.com.ua/goods/images/big/30872883.jpg",
+//         "https://content1.rozetka.com.ua/goods/images/big/30872918.jpg",
+//         "https://content1.rozetka.com.ua/goods/images/big/30872938.jpg"
+//     ]
+// }
+// },
 
 export interface DeviceI {
     name: string
-    images: DeviceImageItme[]
+    colors: string[]
+    images: { [key: string] :string[]}
     id: string | number
     brand: string
     type: string
     price: number
+    faceDescription: string
     description: string
     rating: number
 }
