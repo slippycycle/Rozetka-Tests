2e777e78abf8ece8d6c6c9e8960c58c377d15150
Kiril <vtkkllx2367@gmail.com>
2023-03-06 20:58:52 +0300

    recently viewed works with store / to prevent pre fetching data

--- src/components/RecentlyViewedDevices.tsx
+++ src/components/RecentlyViewedDevices.tsx
@@ -14,7 +14,9 @@ import c from '../styles/RecentlyViewedDevices.module.scss'
 import ViewedeDeviceItem from "./ViewedeDeviceItem";
 import { DeviceI, DeviceId } from "../models/models";
 import { devices } from "../consts";
-import { useAppSelector } from "../store/hooks";
+import { useAppDispatch, useAppSelector } from "../store/hooks";
+import { fetchDevice } from "../API/fetchDevice";
+import { pushViewedDevice } from "../store/features/ViewedDevices.Slice";
 
 
 export default function RecentlyViewedDevices() {
@@ -22,6 +24,7 @@ export default function RecentlyViewedDevices() {
 
   const [devicesId, setDevicesId] = React.useState<DeviceId[]>([])
 
+  const [loading, setLoading] = React.useState<boolean>(true)
 
   const [devicesB, setDevicesB] = React.useState<DeviceI[]>([])
 
@@ -30,57 +33,113 @@ export default function RecentlyViewedDevices() {
   console.log(viewedDevices);
 
 
+  const dispatch = useAppDispatch()
+
   React.useEffect(() => {
     console.log(devicesB)
     const recentlyViewed = JSON.parse(localStorage.getItem('recentlyViewed') as string)
     setDevicesId(recentlyViewed)
-    //http://localhost:3001/products?id=5&id=4
+
+    setLoading(true)
+
+    for (let i = 0; i < recentlyViewed.length; i++) {
+
+      fetchDevice(recentlyViewed[i]).then(res => {
+
+        let device: DeviceI = res as DeviceI
+
+        if (device.id) {
+
+          if (device.images && device.id && device.colors && device.price) {
+            
+
+            dispatch(pushViewedDevice(device))
+
+
+          }
+        } else {
+
+        }
+
+      })
+
+      if (i  == recentlyViewed.length - 1) {
+        setLoading(false)
+      }
+
+    }
+
+
   }, [])
 
   return (
     <div className={c.wrap}>
       <h1>Recently viewed</h1>
-      <div className={c.slider_container}>
+      {loading ?
+        <h1>Loading</h1>
+        :
 
-        <div className={c.fourtneen_slider}>
-          <Swiper
-            slidesPerView={4}
-            spaceBetween={0}
-            freeMode={false}
-            pagination={{
-              clickable: true,
-            }}
-            modules={[FreeMode, Pagination]}
-            className="mySwiper"
-          >
-            {devicesId.map((dev) =>
-              <SwiperSlide key={dev} ><ViewedeDeviceItem id={dev} /></SwiperSlide>
-            )}
+        <div className={c.slider_container}>
 
-          </Swiper>
+          <div className={c.fourtneen_slider}>
+            <Swiper
+              slidesPerView={4}
+              spaceBetween={0}
+              freeMode={false}
+              pagination={{
+                clickable: true,
+              }}
+              modules={[FreeMode, Pagination]}
+              className="mySwiper"
+            >
+              {viewedDevices.map((dev) =>
+                <SwiperSlide key={dev.id} ><ViewedeDeviceItem deviceI={dev} /></SwiperSlide>
+              )}
+
+            </Swiper>
+          </div>
+          <div className={c.treeple_slider}>
+            <Swiper
+              slidesPerView={3}
+              spaceBetween={0}
+              freeMode={false}
+              pagination={{
+                clickable: true,
+              }}
+              modules={[FreeMode, Pagination]}
+              className="mySwiper"
+            >
+              {viewedDevices.map((dev) =>
+                <SwiperSlide key={dev.id} ><ViewedeDeviceItem deviceI={dev} /></SwiperSlide>
+              )}
+
+
+            </Swiper>
+          </div>
+          <div className={c.double_slider}>
+            <Swiper
+              slidesPerView={2}
+              spaceBetween={0}
+              freeMode={false}
+              pagination={{
+                clickable: true,
+              }}
+              modules={[FreeMode, Pagination]}
+              className="mySwiper"
+            >
+              {viewedDevices.map((dev) =>
+                <SwiperSlide key={dev.id} ><ViewedeDeviceItem deviceI={dev} /></SwiperSlide>
+              )}
+
+            </Swiper>
+          </div>
+
+
+
         </div>
-        <div className={c.treeple_slider}>
-          <Swiper
-            slidesPerView={3}
-            spaceBetween={0}
-            freeMode={false}
-            pagination={{
-              clickable: true,
-            }}
-            modules={[FreeMode, Pagination]}
-            className="mySwiper"
-          >
-            {devicesId.map((dev) =>
-              <SwiperSlide key={dev} ><ViewedeDeviceItem id={dev} /></SwiperSlide>
-            )}
 
-          </Swiper>
-        </div>
-        <div className={c.double_slider}></div>
+      }
 
-
-
-      </div>
     </div>
   )
 }
--- src/components/ViewedeDeviceItem.tsx
+++ src/components/ViewedeDeviceItem.tsx
@@ -6,75 +6,29 @@ import { useAppDispatch } from '../store/hooks'
 import c from '../styles/ViewedeDeviceItem.module.scss'
 
 interface ViewedeDeviceItem {
-    id: DeviceId
+    deviceI: DeviceI
 }
 
 
 
-export default function ViewedeDeviceItem({ id }: ViewedeDeviceItem) {
+export default function ViewedeDeviceItem({ deviceI }: ViewedeDeviceItem) {
 
-    const [device, setDevice] = React.useState<DeviceI | null>()
-    const [error, setError] = React.useState<string | null>(null)
-    const [loading, setLoading] = React.useState<boolean>(true)
-
-    const [faceImage, setFaceImage] = React.useState<string>('')
-
-    const dispatch = useAppDispatch()
-
-
-
-    React.useEffect(() => {
-
-        fetchDevice(id).then(res => {
-
-            let device: DeviceI = res as DeviceI
-
-            if (device.id) {
-                setDevice(res as DeviceI)
-                setLoading(false)
-
-                if (device.images && device.id == id && device.colors && device.price) {
-                    let imageUrl: string = device.images[device.colors[0]][0] as string
-                    setFaceImage(imageUrl)
-                    dispatch( pushViewedDevice( device ) )
-                    
-
-                }
-            } else {
-                setError('error')
-            }
-
-        }).catch(err => setError(err))
-
-    }, [])
 
 
 
     return (
-        <>
-            {loading ?
-                <h2>Loading</h2>
-                :
-                <>
-                    {error ?
-                        <h2>{error}</h2>
-                        :
-                        <div className={c.cart}>
-                            <label>
-                                open
-                            </label>
-                            <div className={c.image_container}>
-                                <img alt={`photo ${device?.faceDescription}`} src={faceImage} />
-                            </div>
-                            <div className={c.text_wrap}>
-                                <p>
-                                    {device?.faceDescription}
-                                </p>
-                            </div>
-                        </div>
-                    }
-                </>
-            }
-        </>
+        <div className={c.cart}>
+            <label>
+                open
+            </label>
+            <div className={c.image_container}>
+                <img alt={`photo ${deviceI?.faceDescription}`} src={deviceI.images[deviceI.colors[0]][0]} />
+            </div>
+            <div className={c.text_wrap}>
+                <p>
+                    {deviceI?.faceDescription}
+                </p>
+            </div>
+        </div>
     )
 }
